04929ff89acfd637f03288ed0a0b8c2ced795d83
HashirBaig <hashirbaig456@gmail.com>
2026-09-16 00:14:25 +0200

    added get task list method

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -5,11 +5,29 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { NavTabsList, DataTaskList, type TypeTaskList } from "@/lib/const";
 import { getColumns } from "@/components/DataTable/columns";
 
-import { useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { toast } from "sonner";
 
 function TaskList() {
   const [activeTab, setActiveTab] = useState<string | null>("");
+  const [taskData, setTaskData] = useState<TypeTaskList[]>([]);
+
+  const getTaskData = useCallback(() => {
+    if (activeTab === "active") {
+      const _data = DataTaskList?.filter((item) => !item?.isCompleted);
+      setTaskData(_data);
+    } else if (activeTab === "completed") {
+      const _data = DataTaskList?.filter((item) => item?.isCompleted);
+      setTaskData(_data);
+    } else {
+      setTaskData(DataTaskList);
+    }
+  }, [activeTab]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getTaskData();
+  }, [getTaskData]);
 
   const onTabChange = (value: string | null) => {
     setActiveTab(value);
@@ -52,7 +70,7 @@ function TaskList() {
         </TabsList>
 
         <TabsContent value={activeTab}>
-          <DataTable columns={columns} data={DataTaskList} />
+          <DataTable columns={columns} data={taskData} />
         </TabsContent>
       </Tabs>
     </CardWrapper>
