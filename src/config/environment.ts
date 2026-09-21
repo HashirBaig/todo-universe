cead3a54d8da4b9bb3eee6f52db65914173521d4
HashirBaig <hashirbaig456@gmail.com>
2026-09-21 03:37:25 +0200

    bug fix

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,6 +6,6 @@ const prod = {
   API_URL: "https://todo-universe-nodets.vercel.app/api",
 };
 
-const config = import.meta.env.NODE_ENV === "production" ? prod : dev;
+const config = import.meta.env.MODE === "production" ? prod : dev;
 
 export default config;
