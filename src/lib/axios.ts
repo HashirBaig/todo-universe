2c114075e5ff29ce3bbc271cbe95bca3c67b763f
HashirBaig <hashirbaig456@gmail.com>
2026-09-21 03:56:44 +0200

    removed auth store

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-// import config from "@/config/environment";
+import config from "@/config/environment";
 
 const api = axios.create({
-  baseURL: "https://todo-universe-nodets.vercel.app/api",
+  baseURL: `${config?.API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
