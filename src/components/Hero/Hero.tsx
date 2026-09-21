d16d3e75b0d13c170d5d081983356e98ba2fc3f8
HashirBaig <hashirbaig456@gmail.com>
2026-09-21 13:36:56 +0200

    added user name badge

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,24 @@
+import { useUserStore } from "@/store/userStore";
+import { useMemo } from "react";
+
 function Hero() {
-  const GUEST_USER_INITIALS = "HB";
+  const username = useUserStore((state) => state.username);
+
+  const userInitials = useMemo(() => {
+    if (!username) return "";
+
+    const parts = username.split("-");
+    const first = parts[0]?.charAt(0).toUpperCase() ?? "";
+    const second = parts[1]?.charAt(0).toUpperCase() ?? "";
+
+    return `${first}${second}`;
+  }, [username]);
 
   return (
     <header className="flex items-center justify-end">
-      <div className="p-2 cursor-pointer border-2 border-blue-400/20 hover:bg-blue-400/5 rounded-full">
-        <h1 className="text-blue-400 text-2xl font-semibold text-center">
-          {GUEST_USER_INITIALS}
+      <div className="p-2 cursor-pointer border-3 bg-blue-800/70 border-blue-800/70 rounded-full">
+        <h1 className="text-blue-100 text-2xl font-semibold text-center">
+          {userInitials}
         </h1>
       </div>
     </header>
